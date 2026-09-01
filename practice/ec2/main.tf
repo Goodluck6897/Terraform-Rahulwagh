e77532f58096a2c863d6e728b17c96cc478267f6
venkatadry <mullapudi67@gmail.com>
2026-08-31 22:31:20 -0600

    x

--- practice/ec2/main.tf
+++ practice/ec2/main.tf
@@ -3,8 +3,10 @@
 # ==========================================
 provider "aws" {
   region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  shared_credentials_files = ["/Users/vm/Documents/AWS/credentials"]
+  #/Users/vm/Documents/AWS
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
 }
 
 # ==========================================
