b93a05c28cbd3030350186a7b2194d1b08bdf74e
venkatadry <mullapudi67@gmail.com>
2026-09-11 22:38:53 -0600

    outputs

---
+++ practice/outputs/main.tf
@@ -0,0 +1,17 @@
+# ==========================================
+# 2. EC2 INSTANCE RESOURCE
+# ==========================================
+resource "aws_instance" "my_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    # Name = local.instance_name ->This also works
+    Name = "${local.instance_name}-local"
+
+  }
+}
+
+locals {
+  instance_name = "My EC2 Instance"
+}
---
+++ practice/outputs/outputs.tf
@@ -0,0 +1,3 @@
+output "ec2-instance" {
+  value = aws_instance.my_ec2.id
+}
